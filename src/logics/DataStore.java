1c576381a7d8dea25a495c4d5d5c301e31c567ab
kasungayand <kasungayand@yahoo.com>
2015-03-30 16:44:44 +0530

    Create Class Data Store..

--- src/logics/DataStore.java
+++ src/logics/DataStore.java
@@ -4,4 +4,59 @@ package logics;
 
 public class DataStore {
     
+    private Long lotNo;
+    private String fName;
+    private String lName;
+    private Long price;
+    private Long sqFeet;
+    private Integer noOfRoom;
+
+    public Long getLotNo() {
+        return lotNo;
+    }
+
+    public void setLotNo(Long lotNo) {
+        this.lotNo = lotNo;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getSqFeet() {
+        return sqFeet;
+    }
+
+    public void setSqFeet(Long sqFeet) {
+        this.sqFeet = sqFeet;
+    }
+
+    public Integer getNoOfRoom() {
+        return noOfRoom;
+    }
+
+    public void setNoOfRoom(Integer noOfRoom) {
+        this.noOfRoom = noOfRoom;
+    }
+      
 }
